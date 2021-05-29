34378c125a7ab4510effbb77203f0dcb2cdefcc5
syibro98 <syibrobagus@gmail.com>
2021-05-29 10:35:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118052/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.quiz10118052;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+ * tanggal pengerjaan : 29/05/2021
+ * NIM: 10118052
+ * Nama: Bagus Syibro Malisi
+ * Kelas: IF-2
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
